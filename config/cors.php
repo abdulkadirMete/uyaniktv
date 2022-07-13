3e53b8dc43c517a1c3b50eb8f77bb4b6244fdd32
Kadir Mete <abdulkadir.mete@bil.omu.edu.tr>
2022-07-13 14:37:08 +0300

    .

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['*','/sanctum/csrf-cookie'],
 
     'paths' => ['*'],
 
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
