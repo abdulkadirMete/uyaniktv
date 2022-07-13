b743e11c397ad09dd5096321f9817565d1ad0e9a
Kadir Mete <abdulkadir.mete@bil.omu.edu.tr>
2022-07-13 14:09:08 +0300

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,10 +9,6 @@
 | which serves as the "glue" for all the components of Laravel, and is
 | the IoC container for the system binding all of the various parts.
 |
-*/
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
 
 
 $app = new Illuminate\Foundation\Application(
