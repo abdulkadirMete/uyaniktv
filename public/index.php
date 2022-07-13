c5ff626864fcb78537e748d73a77c36ab1df130c
Kadir Mete <abdulkadir.mete@bil.omu.edu.tr>
2022-07-13 14:03:45 +0300

    .

--- public/index.php
+++ public/index.php
@@ -55,18 +55,3 @@ $response = $kernel->handle(
 $kernel->terminate($request, $response);
 
 
-$allowedOrigins = array(
-    'https://uyaniktv.herokuapp.com/', // Laravel API Domain
-    'https://master--uyaniktv.netlify.app/' // VueJS CLient
- );
- if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
-    foreach ($allowedOrigins as $allowedOrigin) {
-       if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
-          header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-          header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-          header('Access-Control-Max-Age: 1000');
-          header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-          break;
-       }
-    }
- }
--- routes/api.php
+++ routes/api.php
@@ -5,24 +5,14 @@ use App\Http\Controllers\ChannelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
-
-
-Route::middleware(['cors'])->group(function () {
-
-    
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/user', function (Request $request) {
-            return $request->user();
-        });
-        Route::post('/cikis', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
     });
-
-    Route::post('/kaydol', [AuthController::class, 'register']);
-    Route::post('/giris', [AuthController::class, 'login']);
-    Route::get('/kanallar', [ChannelController::class, 'getAllChannels']);
-    Route::get('/canli-izle/{id}', [ChannelController::class, 'getSingleChannel']);
+    Route::post('/cikis', [AuthController::class, 'logout']);
 });
+
+Route::post('/kaydol', [AuthController::class, 'register']);
+Route::post('/giris', [AuthController::class, 'login']);
+Route::get('/kanallar', [ChannelController::class, 'getAllChannels']);
+Route::get('/canli-izle/{id}', [ChannelController::class, 'getSingleChannel']);
