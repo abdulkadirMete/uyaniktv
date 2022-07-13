90394c5761f3482b9784016f035146a3eaa3cfc2
Kadir Mete <abdulkadir.mete@bil.omu.edu.tr>
2022-07-13 13:19:06 +0300

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         \Fruitcake\Cors\HandleCors::class, # this line
+        'cors'          => \App\Http\Middleware\Cors::class, // added
+
 
 
     ];
--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+
+
+
+
+
+Route::middleware(['cors'])->group(function () {
+
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('/cikis', [AuthController::class, 'logout']);
     });
-    Route::post('/cikis', [AuthController::class, 'logout']);
+
+    Route::post('/kaydol', [AuthController::class, 'register']);
+    Route::post('/giris', [AuthController::class, 'login']);
+    Route::get('/kanallar', [ChannelController::class, 'getAllChannels']);
+    Route::get('/canli-izle/{id}', [ChannelController::class, 'getSingleChannel']);
 });
-
-
-Route::post('/kaydol', [AuthController::class, 'register']);
-Route::post('/giris', [AuthController::class, 'login']);
-Route::get('/kanallar', [ChannelController::class, 'getAllChannels']);
-Route::get('/canli-izle/{id}', [ChannelController::class, 'getSingleChannel']);
